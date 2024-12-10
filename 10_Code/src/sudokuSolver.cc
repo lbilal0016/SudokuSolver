9d44ec369ef5a15db37c1cadb9a5310ff79aeabf
leventbilal <levent.bilal@intive.com>
2024-12-10 22:35:22 +0100

    class design ongoing

--- 10_Code/src/sudokuSolver.cc
+++ 10_Code/src/sudokuSolver.cc
@@ -1,2 +1,51 @@
+#include <iostream>
 #include "sudokuSolver.h"
 
+
+sudokuSolver::sudokuSolver(BoardType &board, bool isSolved) : _board(board), _isSolved(isSolved) {
+    //  ToDo: Some False argument check
+    
+    _originalsMarked = false;
+
+    _markedElements.resize(NUM_ROWS, std::vector<bool>(NUM_COLUMNS, false));    //  shaping NUM_ROWS x NUM_COLUMNS marked elements vector
+    _possibleValues.resize(NUM_ROWS, std::vector<std::vector<int>>(NUM_COLUMNS, std::vector<int>(NUM_POSSIBILITIES, 0)));   //  shaping NUM_ROWS x NUM_COLUMNS x NUM_POSSIBILITIES possible values vector
+    //  shaping the marked elements vector
+    for(size_t i = 0;  i < NUM_ROWS; ++i){
+        for(size_t j = 0; j < NUM_COLUMNS; ++j){
+            _markedElements[i][j] = false;
+        }
+    }
+
+    markOriginals();
+}
+
+sudokuSolver::sudokuSolver(){
+    //  Shape the board and board marker vectors
+    for(size_t i = 0;  i < NUM_ROWS; ++i){
+        for(size_t j = 0; j < NUM_COLUMNS; ++j){
+            _board[i][j] = 0; 
+            _markedElements[i][j] = false;
+        }
+    }
+}
+
+BoardType sudokuSolver::solvePuzzle(BoardType &BoardType){
+    //  ToDo:   Create a validity check against BoardType
+
+    if(_originalsMarked != true){
+    //  Marking down the original elements which are parts of the puzzle and not the solution
+    markOriginals();
+    }
+
+}
+
+void sudokuSolver::markOriginals(){
+    //  ToDo: Mark original elements
+    for(size_t i = 0; i < NUM_ROWS; ++i){
+        for(size_t j = 0; j < NUM_COLUMNS; ++j){
+
+        }
+    }
+    //  Finish: set the flag
+    _originalsMarked = true;
+}
--- 10_Code/src/sudokuSolver.h
+++ 10_Code/src/sudokuSolver.h
@@ -1,4 +1,49 @@
 #pragma once
-#include "sudokuSolver.h"
+#include <cstdint>
+#include <vector>
 
-#define CONSTANTS 5
+#define NUM_ROWS 9
+#define NUM_COLUMNS 9
+#define NUM_POSSIBILITIES 9
+
+using BoardType = std::vector<std::vector<int>>;
+using BoardMarker = std::vector<std::vector<bool>>;
+
+struct BoardIndex
+{
+    int i;  //  first dimension of BoardType vector
+    int j;  //  second dimension of BoardType vector
+};
+
+
+class sudokuSolver{
+    public:
+        sudokuSolver(BoardType &board, bool isSolved);
+        sudokuSolver();
+
+        ~sudokuSolver();
+
+        //  This function is for solving a non-finished sudoku
+        BoardType solvePuzzle(BoardType &BoardType);
+
+        //  This function is for completed sudokus, whose correctness is checked
+        bool checkPuzzle();
+
+    private:
+        BoardType _board;
+        std::vector<BoardType> _possibleValues;
+        BoardMarker _markedElements;
+
+        bool _isSolved;
+        bool _originalsMarked;
+        
+
+        //  This function returns vector index of a target element whose row and column is known
+        BoardIndex getElementIndex(int targetRow, int targetColumn);
+
+        //  This function marks elements from sudoku itself, such that they cannot be changed
+        void markOriginals();
+
+        //  This function checks whether an element is an original element or a solved element
+        bool isOriginal();
+};
