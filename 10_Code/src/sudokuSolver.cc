8a29d7611cccacbd6c0ea639cb6466aedcdbf6b2
leventbilal <levent.bilal@intive.com>
2024-12-11 21:38:30 +0100

    design of many helper functions

--- 10_Code/src/sudokuSolver.cc
+++ 10_Code/src/sudokuSolver.cc
@@ -7,29 +7,21 @@ sudokuSolver::sudokuSolver(BoardType &board, bool isSolved) : _board(board), _is
     
     _originalsMarked = false;
 
-    _markedElements.resize(NUM_ROWS, std::vector<bool>(NUM_COLUMNS, false));    //  shaping NUM_ROWS x NUM_COLUMNS marked elements vector
-    _possibleValues.resize(NUM_ROWS, std::vector<std::vector<int>>(NUM_COLUMNS, std::vector<int>(NUM_POSSIBILITIES, 0)));   //  shaping NUM_ROWS x NUM_COLUMNS x NUM_POSSIBILITIES possible values vector
-    //  shaping the marked elements vector
-    for(size_t i = 0;  i < NUM_ROWS; ++i){
-        for(size_t j = 0; j < NUM_COLUMNS; ++j){
-            _markedElements[i][j] = false;
-        }
-    }
+    _possibleValues.resize(NUM_ROWS, std::vector<uint16_t>(NUM_COLUMNS, 0));   //  shaping NUM_ROWS x NUM_COLUMNS elements possbility vector
 
     markOriginals();
 }
 
 sudokuSolver::sudokuSolver(){
-    //  Shape the board and board marker vectors
-    for(size_t i = 0;  i < NUM_ROWS; ++i){
-        for(size_t j = 0; j < NUM_COLUMNS; ++j){
-            _board[i][j] = 0; 
-            _markedElements[i][j] = false;
-        }
-    }
+    _originalsMarked = false;
+    _isSolved = false;
+
+    //  shaping member vectors
+    _board.resize(NUM_ROWS, std::vector<int>(NUM_COLUMNS, 0));  //  shaping NUM_ROWS x NUM_COLUMNS board vector
+    _possibleValues.resize(NUM_ROWS, std::vector<uint16_t>(NUM_COLUMNS, 0));   //  shaping NUM_ROWS x NUM_COLUMNS elements possbility vector
 }
 
-BoardType sudokuSolver::solvePuzzle(BoardType &BoardType){
+BoardType sudokuSolver::solvePuzzle(BoardType &Board){
     //  ToDo:   Create a validity check against BoardType
 
     if(_originalsMarked != true){
@@ -37,15 +29,91 @@ BoardType sudokuSolver::solvePuzzle(BoardType &BoardType){
     markOriginals();
     }
 
+    //  After all other operations, return the board
+    return _board;
+}
+
+BoardIndexRange sudokuSolver::getIndexRange(int boardOneNinth){
+    BoardIndexRange indexRange;
+    if(!(0 < boardOneNinth < 10)){
+        throw std::invalid_argument("Please give a board ninth between [1-9]");
+    }
+
+    //  determine row range
+    if(boardOneNinth <= 3){
+        indexRange.rowFirst = 0;
+        indexRange.rowLast = 2;
+    }else if(boardOneNinth <= 6){
+        indexRange.rowFirst = 3;
+        indexRange.rowLast = 5;
+    }else{
+        indexRange.rowFirst = 6;
+        indexRange.rowLast = 8;
+    }
+
+    //  determine column range
+    if(boardOneNinth == 1 || boardOneNinth == 4 || boardOneNinth == 7){
+        indexRange.columnFirst = 0;
+        indexRange.columnLast = 2;
+    }else if(boardOneNinth == 2 || boardOneNinth == 5 || boardOneNinth == 8){
+        indexRange.columnFirst = 3;
+        indexRange.columnLast = 5;
+    }else{
+        indexRange.columnFirst = 6;
+        indexRange.columnLast = 8;
+    }
+
+    return indexRange;
 }
 
 void sudokuSolver::markOriginals(){
     //  ToDo: Mark original elements
     for(size_t i = 0; i < NUM_ROWS; ++i){
         for(size_t j = 0; j < NUM_COLUMNS; ++j){
-
+            if(_board[i][j] != 0){
+                //  set market bit for elements corresponding original elements on the sudoku board
+                _possibleValues[i][j] |= MARKER_BIT;
+            }else{
+                //  set all possible bits for elements not corresponding original elements on the sudoku board
+                _possibleValues[i][j] |= ALLBITS;
+            }
         }
     }
     //  Finish: set the flag
     _originalsMarked = true;
+}
+
+bool sudokuSolver::isOriginal(int row, int column){
+    //  If marker bit is set, the value is original
+    return _possibleValues[row][column] & MARKER_BIT != 0;
+}
+
+void sudokuSolver::setPossibleValue(int row, int column, int value){
+    //  set bit number value - 1 (0th bit corresponds to value 1)
+    if(_possibleValues[row][column] & MARKER_BIT != 0){
+        throw IllegalOperationException("Exception occured: Attempt to change an original entry.");
+    }else{ 
+        _possibleValues[row][column] = _possibleValues[row][column] | (1 << (value - 1));
+    }
+}
+
+void sudokuSolver::removeImpossibleValue(int row, int column, int value){
+    //  reset bit number value - 1 (0th bit corresponds to value 1)
+    if(_possibleValues[row][column] & MARKER_BIT != 0){
+        throw IllegalOperationException("Exception occured: Attempt to change an original entry.");
+    }else{ 
+        _possibleValues[row][column] = _possibleValues[row][column] & ~(1 << (value - 1));
+    }
+}
+
+void sudokuSolver::checkOneNinths(){
+    BoardIndexRange indexRange;
+    for(int i = 1; i <= NUM_NINTHS; ++i){
+        indexRange = getIndexRange(i);
+        for(int j = indexRange.rowFirst; j <= indexRange.rowLast; ++j){
+            for(int k = indexRange.columnFirst; k <= indexRange.columnLast; ++k){
+                
+            }
+        }
+    }
 }
--- 10_Code/src/sudokuSolver.h
+++ 10_Code/src/sudokuSolver.h
@@ -1,18 +1,35 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include <cstdint>
+#include <stdexcept>
 
 #define NUM_ROWS 9
 #define NUM_COLUMNS 9
-#define NUM_POSSIBILITIES 9
+#define NUM_NINTHS 9
+
+constexpr uint16_t BITPOS_1 = 0b0000000000000001;
+constexpr uint16_t BITPOS_2 = 0b0000000000000010;
+constexpr uint16_t BITPOS_3 = 0b0000000000000100;
+constexpr uint16_t BITPOS_4 = 0b0000000000001000;
+constexpr uint16_t BITPOS_5 = 0b0000000000010000;
+constexpr uint16_t BITPOS_6 = 0b0000000000100000;
+constexpr uint16_t BITPOS_7 = 0b0000000001000000;
+constexpr uint16_t BITPOS_8 = 0b0000000010000000;
+constexpr uint16_t BITPOS_9 = 0b0000000100000000;
+constexpr uint16_t ALLBITS = 0b0000000111111111;
+constexpr uint16_t MARKER_BIT = 0b1000000000000000;
+constexpr uint16_t VALUE_FOUND_BIT = 0b0100000000000000;
 
 using BoardType = std::vector<std::vector<int>>;
-using BoardMarker = std::vector<std::vector<bool>>;
+using BoardPosType = std::vector<std::vector<uint16_t>>;
 
-struct BoardIndex
+struct BoardIndexRange
 {
-    int i;  //  first dimension of BoardType vector
-    int j;  //  second dimension of BoardType vector
+    int rowFirst;  //  first row index for one ninth of board
+    int rowLast;  //  last row index for one ninth of board
+    int columnFirst; //  first column index for one ninth of board
+    int columnLast; //  last column index for one ninth of board
 };
 
 
@@ -24,26 +41,45 @@ class sudokuSolver{
         ~sudokuSolver();
 
         //  This function is for solving a non-finished sudoku
-        BoardType solvePuzzle(BoardType &BoardType);
+        BoardType solvePuzzle(BoardType &Board);
 
         //  This function is for completed sudokus, whose correctness is checked
         bool checkPuzzle();
 
-    private:
+    protected:
         BoardType _board;
-        std::vector<BoardType> _possibleValues;
-        BoardMarker _markedElements;
+        BoardPosType _possibleValues;
 
         bool _isSolved;
         bool _originalsMarked;
         
-
         //  This function returns vector index of a target element whose row and column is known
-        BoardIndex getElementIndex(int targetRow, int targetColumn);
+        BoardIndexRange getIndexRange(int boardOneNinth);
 
         //  This function marks elements from sudoku itself, such that they cannot be changed
         void markOriginals();
 
         //  This function checks whether an element is an original element or a solved element
-        bool isOriginal();
+        bool isOriginal(int row, int column);
+
+        //  This function sets a possible value to a given square
+        void setPossibleValue(int row, int column, int value);
+
+        //  This function removes an impossible value from a given square
+        void removeImpossibleValue(int row, int column, int value);
+
+        //  This function checks one-ninth regions and marks possible values to empty squares
+        void checkOneNinths();
+};
+
+class IllegalOperationException : public std::exception{
+    public:
+        explicit IllegalOperationException(const std::string& message) : _message(message){
+        }
+        
+        const char * what() const noexcept override{
+            return _message.c_str();
+        }
+    private:
+        std::string _message;
 };
