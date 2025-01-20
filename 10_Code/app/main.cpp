e3e60e1855226713505334d8224cd07d5cdc48a9
lb0016 <leventbilal.16@outlook.com>
2025-01-20 22:01:26 +0100

    Main program was adapted to accept sudoku puzzle as main program argument.

--- 10_Code/app/main.cpp
+++ 10_Code/app/main.cpp
@@ -1,33 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 #include "sudokuSolver.h"
 #include "DoublyLinkedList.h"
 
-int main(){
-    // Example input matrix
-    std::vector<std::vector<int>> matrix = {
-        {0, 0, 1, 0, 1, 1, 0},
-        {1, 0, 0, 1, 0, 0, 1},
-        {0, 1, 1, 0, 0, 1, 0},
-        {1, 0, 0, 1, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0, 1},
-        {0, 0, 0, 1, 1, 0, 1}
-    };
+int main(int argc, char* argv[]){
+    //  input argument error handling
+    if(argc < 2){
+        std::cerr << "Input argument usage wrong.\n"
+        <<  "Correct usage: sudokuSolver.exe 530...000" << std::endl;
+        return 1;
+    }
 
-    // Create DLX instance
-    DLX dlx(matrix);
+    std::string inputArg = argv[1];
+    if(inputArg.size() != NUM_CELLS){
+        std::cerr << "Program argument should contain 81 numbers for each sudoku cell.\n";
+        return 1;
+    }
 
-    //  test the cover and uncover functions
-    //dlx.testCoverUncover(0);
+    std::vector<int> parsedInput;
+    for(char ch : inputArg){
+        //  Error handling for false character input
+        if(ch < '0' || ch > '9'){
+            std::cerr << "There is an invalid character in input argument: "
+            << ch << std::endl
+            << "Program aborted...\n";
+        }
 
-    // Print the structure
-    /*
-    std::cout << "\nPrinting the original DLX Structure..." << std::endl;
-    std::cout << "=========================================" << std::endl;
-    dlx.print();
-    */
-   
-    dlx.search(0);
+        //  add character to parsedInput vector using ASCII code
+        parsedInput.push_back(ch - '0');
+    }
+
+
+ std::cout << "Input argument vector:\n======================\n";
+for(int i = 0; i < parsedInput.size(); ++i){
+    if(i % 9 == 0) std::cout << "{ "; // Line beginning
+    std::cout << parsedInput[i];
+    if(i % 9 != 8) std::cout << ", "; // Adding comma
+    else std::cout << " }"; // Line end
+    if(i % 9 == 8) std::cout << "\n"; // Newline
+}
+
 
     return 0;
 }
